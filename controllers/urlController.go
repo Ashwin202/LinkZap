89514492d2c12ca11d5aece0cdd65c7714d6ace6
Ashwin <ashes192000@gmail.com>
2023-12-25 15:44:41 +0530

    commenetd unwanted variables

--- controllers/urlController.go
+++ controllers/urlController.go
@@ -5,15 +5,15 @@ import (
 	"url-shortner/db"
 
 	"github.com/gin-gonic/gin"
-	"go.mongodb.org/mongo-driver/bson"
+	// "go.mongodb.org/mongo-driver/bson"
 )
 
 func GetURL(c *gin.Context) {
-	userData := bson.D{{"fullName", "User 1"}, {"age", 30}}
-	result, _ := db.InsertOneDB("ashwin", "go", userData)
+	// userData := bson.D{{"fullName", "User 1"}, {"age", 30}}
+	// result, _ := db.InsertOneDB("ashwin", "go", userData)
 	fmt.Println(&c)
 	c.JSON(200, gin.H{
-		"message": &result,
+		"message": "&result",
 	})
 }
 
@@ -21,5 +21,6 @@ func AddURL(c *gin.Context) {
 	result, _ := db.InsertOneDB("ashwin", "go", "userData")
 	c.JSON(200, gin.H{
 		"message": &result,
+        
 	})
 }
