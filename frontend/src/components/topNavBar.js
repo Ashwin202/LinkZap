70906ff47dce165f7533b71e206e0f1f2e7229dc
Ashwin <ashwinm.045@gmail.com>
2024-08-11 17:50:00 +0530

    Added header image in navbar

--- frontend/src/components/topNavBar.js
+++ frontend/src/components/topNavBar.js
@@ -5,7 +5,7 @@ function TopNavBar() {
   return (
     <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary nav-bar">
       <Container>   
-        <Navbar.Brand href="#">LinkZap</Navbar.Brand>
+        <Navbar.Brand href="#" className='inter-xl'> <img src="/linkzap.png" width="30" height="30" className="d-inline-block align-top" alt="Logo" /> LinkZap</Navbar.Brand>
             <div className='inter'>Hi, <span className='text-blue'>Peeps</span></div>
       </Container>
     </Navbar>
